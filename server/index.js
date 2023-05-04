22e79d8078d3016dbff115c9c965c443b5a55638
Ian Dodds <ianmitchelldodds@gmail.com>
2023-05-04 11:10:52 -0400

    Add user model and connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const User = require('./models/user.model');
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+mongoose.connect('mongodb://localhost:27017/dignify');
+
+app.post('/api/register', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: 'ok' });
+  } catch (error) {
+    // console.log(error);
+    res.json({ status: 'error' });
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  if (user) {
+    return res.json({ status: 'ok', user: true });
+  } else {
+    return res.json({ status: 'error', user: false });
+  }
 });
 
 app.listen(3000, () => {
